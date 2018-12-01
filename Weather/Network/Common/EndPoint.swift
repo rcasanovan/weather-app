2c5a864d13d97987561408ef690f0f568e097843
Ricardo Casanova <ricardo@pijp.eu>
2018-12-01 13:04:32 +0100

    Add app id to the endpoint

--- Weather/Network/Common/EndPoint.swift
+++ Weather/Network/Common/EndPoint.swift
@@ -19,10 +19,12 @@ private struct Url {
     
     static let baseUrl: String = "api.openweathermap.org/data/"
     static let version: String = "2.5/"
+    static let appID: String = "28bc6d7ace6e643065fd95756fae8b9c"
     
     struct Fields {
         static let latitude: String = "lan"
         static let longitude: String = "long"
+        static let appID: String = "APPID"
     }
     
 }
@@ -33,7 +35,7 @@ enum Endpoint: EndpointProtocol {
     var rawValue: String {
         switch self {
         case .getWeatherWith(let latitude, let longitude):
-            return "/forecast?\(Url.Fields.latitude)=\(latitude)&\(Url.Fields.longitude)=\(longitude)"
+            return "/forecast?\(Url.Fields.latitude)=\(latitude)&\(Url.Fields.longitude)=\(longitude)&\(Url.Fields.appID)=\(Url.appID)"
         }
     }
     
