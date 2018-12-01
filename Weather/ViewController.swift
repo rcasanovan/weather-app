b96a947efc554052dd03cc462897f475fb40ce0d
Ricardo Casanova <ricardo@pijp.eu>
2018-12-01 12:35:10 +0100

    Add initial commit
    Add initial Xcode project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ricardo Casanova on 01/12/2018.
+//  Copyright © 2018 Pijp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
