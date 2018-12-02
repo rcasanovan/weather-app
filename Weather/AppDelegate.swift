2c1718f49792bcb7fc215d8f94684bfe6d85acd1
Ricardo Casanova <ricardo@pijp.eu>
2018-12-02 19:54:38 +0100

    Reorganise folders

--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureFirebase()
         configureNavigationBar()
         showInitialView()
         addObservers()
@@ -104,5 +106,13 @@ extension AppDelegate  {
         }
     }
 
+}
+
+extension AppDelegate {
+    
+    private func configureFirebase() {
+        FirebaseApp.configure()
+    }
+    
 }
 
