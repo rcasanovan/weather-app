e2a9ddcae4dc347d75e79722b8b6e515d571457e
Ricardo Casanova <ricardo@pijp.eu>
2018-12-02 15:40:04 +0100

    Add GeneralMessage view

---
+++ Weather/Extensions/UIEdgeInsets/UIEdgeInsets.swift
@@ -0,0 +1,24 @@
+//
+//  UIEdgeInsets.swift
+//  Weather
+//
+//  Created by Ricardo Casanova on 02/12/2018.
+//  Copyright © 2018 Pijp. All rights reserved.
+//
+
+import UIKit
+
+extension UIEdgeInsets {
+    
+    static var safeAreaInsets: UIEdgeInsets = {
+        guard let window = (UIApplication.shared.delegate as? AppDelegate)?.window else { return UIEdgeInsets.zero }
+        let insets: UIEdgeInsets
+        if #available(iOS 11.0, *) {
+            insets = window.safeAreaInsets
+        } else {
+            insets = UIEdgeInsets.zero
+        }
+        return insets
+    }()
+    
+}
